57da10a7791ed464410d0e23ae564658aced17a3
Cleverson Purkot <137345325+CPurkot86-hub@users.noreply.github.com>
2023-10-17 20:42:43 -0300

    criando relatorios e dash

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ykdh3a',
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/",
     setupNodeEvents(on, config) { 
